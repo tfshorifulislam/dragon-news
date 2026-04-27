56336351c911c7801d4142ff0544a64583446972
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-27 15:49:26 +0600

    right side bar done

--- src/components/NewsPage/RightSideBar.jsx
+++ src/components/NewsPage/RightSideBar.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
 
 const RightSideBar = () => {
     return (
         <div>
-            <h1 className='text-black font-semibold mb-6 '>
+            <h1 className='text-black text-xl font-semibold mb-6 '>
                 Login With
             </h1>
-            <div className='space-y-4'>
-                <button className="btn bg-white text-black border-[#e5e5e5] rounded-sm ">
+            <div className='space-y-4 mb-8'>
+                <button className="btn bg-white w-full text-black border-[#e5e5e5] rounded-sm ">
                     <FaGoogle /> Login with Google
                 </button>
-                <button className="btn bg-white text-black border-[#e5e5e5] rounded-sm ">
+                <button className="btn w-full bg-white text-black border-[#e5e5e5] rounded-sm ">
                     <FaGithub /> Login with Github
                 </button>
+            </div>
+
+            <div>
+                <p className='font-semibold text-xl mb-5 text-[#403F3F]'>
+                    Find Us On
+                </p>
+                <div className='border border-[#e5e5e5]'>
+                    <button className="btn py-6 bg-white w-full text-black border-[#e5e5e5] rounded-sm ">
+                        <FaFacebook /> Login with Facebook
+                    </button><button className="btn py-6 bg-white w-full text-black border-[#e5e5e5] rounded-sm ">
+                        <FaInstagramSquare /> Login with Instagram
+                    </button><button className="btn py-6 bg-white w-full text-black border-[#e5e5e5] rounded-sm ">
+                        <FaTwitterSquare /> Login with Twitter
+                    </button>
+                </div>
             </div>
 
         </div>
